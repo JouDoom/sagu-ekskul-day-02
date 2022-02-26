ff5d2e432a43a9b36e23f8158b8dea803fd221d4
Jou Doom <91768022+JouDoom@users.noreply.github.com>
2022-02-26 11:31:46 +0900

    push project 03

---
+++ project03/comments.php
@@ -0,0 +1,14 @@
+<?php 
+    // this is my first single line commment in php
+    echo "single line comment using '//' <br><br> ";
+
+    # this also a single line comment
+    echo "single line comment using this'#' <br><br> ";
+
+
+    /* this is 
+    multiple line comment 
+    in php 
+    */
+    echo "multiple line comment in php using '/*...*/' "
+?>
