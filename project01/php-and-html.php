72492b8c24fe0091d3341bf989e8824204ce930b
Jou Doom <91768022+JouDoom@users.noreply.github.com>
2022-02-26 10:48:28 +0900

    push project 01

---
+++ project01/php-and-html.php
@@ -0,0 +1,18 @@
+<?php 
+    print("
+        <html>
+            <head>
+                <title>PHP and HTMl</title>
+            </head>
+            
+            <body>
+
+                <h1>Hello SAGU!</h1>
+                <p>We are learning PHP Codes.</p>
+
+            </body>
+
+        </html>
+      
+    ");
+?>
